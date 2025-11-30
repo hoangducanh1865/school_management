53b17219e1f78249481057dcecb005bff225ba9a
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-11-30 22:42:40 +0700

    feat: add Next.js Tailwind responsive layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +17,16 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div>dashboard{children}</div>
+      <div className="h-screen flex">
+        {/* LEFT */}
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+          <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+            <Image src="/logo.png" alt="logo" width={32} height={32} />
+            <span className="hidden lg:block">SoICT-HUST</span>
+          </Link>
+        </div>
+        {/* RIGHT */}
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+      </div>
   );
 }
